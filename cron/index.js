990a69b07ffcd2620d6f618e93e93a0ed9b78025
Carlos Saito <c.saito.style@gmail.com>
2019-07-10 14:36:37 +0200

    Get course rounds from Kopps to apply antagna removal

--- cron/index.js
+++ cron/index.js
@@ -2,18 +2,20 @@ const { scheduleJob } = require('node-schedule')
 const log = require('../logger')
 const Period = require('../lib/Period')
 const getEnrollments = require('../lib/getEnrollments')
+const Kopps = require('../lib/Kopps')
 
 // "0 5 * * *" = "Every day at 5:00"
 const INTERVAL = process.env.INTERVAL || '0 5 * * *'
 
 const PERIOD = Period(process.env.PERIOD || '2019-HT-P1')
 
+const kopps = Kopps()
+
 async function sync () {
   // Start a new context
-  log.context({ start_time: new Date() }, () => {
+  log.context({ start_time: new Date() }, async () => {
     log.info(`Starting sync for period ${PERIOD}`)
-    getEnrollments.toRemoveAntagna(PERIOD.prevPeriod())
-
+    await getEnrollments.toRemoveAntagna(PERIOD.prevPeriod(), { kopps })
     log.info('Finishing sync...')
   })
 }
