afb523f6202ec03f9077a01138554a946cfd3521
Carlos Saito <c.saito.style@gmail.com>
2019-07-10 21:30:51 +0200

    Improve "debug" logs

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const cron = require('./cron')
 const log = require('./logger')
 
 log.createLogger({
-  name: 'lms-antagna'
+  name: 'lms-antagna',
+  level: process.env.NODE_ENV === 'development' ? 'trace' : 'info'
 })
 
 log.context({ request_type: 'cron' }, () => {
