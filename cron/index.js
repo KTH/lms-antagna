9949fa4bec51042f4545e050d25901e81b5a6ab9
Carlos Saito <c.saito.style@gmail.com>
2019-07-10 12:51:48 +0200

    Add a "Period" object

--- cron/index.js
+++ cron/index.js
@@ -1,13 +1,16 @@
 const { scheduleJob } = require('node-schedule')
 const log = require('../logger')
+const Period = require('../lib/Period')
 
 // "0 5 * * *" = "Every day at 5:00"
 const INTERVAL = process.env.INTERVAL || '0 5 * * *'
 
+const PERIOD = Period(process.env.PERIOD || '2019-HT-P1')
+
 async function sync () {
   // Start a new context
   log.context({ start_time: new Date() }, () => {
-    log.info('Starting sync...')
+    log.info(`Starting sync for period ${PERIOD}`)
     log.info('Finishing sync...')
   })
 }
