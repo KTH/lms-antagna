3196e93a5ebf328874363033da4257ce5541d1c4
Carlos Saito <carlos@saito.style>
2019-07-22 15:42:58 +0200

    Add a "catch-all" route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const prefix = process.env.PROXY_PATH || ''
 
+app.use((req, res) => {
+  res.send('Hello antagna')
+})
+
 app.get(prefix + '/about', (req, res) => {
   res.send('Hello antagna')
 })
