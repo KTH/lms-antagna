844feff803769c94e7d95d591b6afba7550505c5
Carlos Saito <carlos@saito.style>
2020-08-25 16:58:17 +0200

    Correct style

--- scripts/write-to-file.js
+++ scripts/write-to-file.js
@@ -29,15 +29,18 @@ const getEnrollments = require('../lib/get-enrollments')
 async function start () {
   console.log(
     'This is the "write-to-file" script.\n\n' +
-    `It will generate CSV files for REMOVING antagna for period ${CURRENT_PERIOD.prevPeriod()}\n` +
-    `and ADDING antagna for period ${CURRENT_PERIOD}\n\n` +
-    'Please change the env variable "PERIOD" if you want different periods to be handled'
+      `It will generate CSV files for REMOVING antagna for period ${CURRENT_PERIOD.prevPeriod()}\n` +
+      `and ADDING antagna for period ${CURRENT_PERIOD}\n\n` +
+      'Please change the env variable "PERIOD" if you want different periods to be handled'
   )
 
   const enr1 = await getEnrollments.toRemoveAntagna(CURRENT_PERIOD.prevPeriod())
   const enr2 = [] //await getEnrollments.toAddAntagna(CURRENT_PERIOD)
 
-  file.writeEnrollments(`/tmp/enrollments-antagna-${CURRENT_PERIOD}.csv`, [...enr1, ...enr2])
+  file.writeEnrollments(`/tmp/enrollments-antagna-${CURRENT_PERIOD}.csv`, [
+    ...enr1,
+    ...enr2
+  ])
 }
 
 start()
